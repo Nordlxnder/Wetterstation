5cd95c9e2553fbd899a6a5fa766d1ca6264b8b82
Nordlxnder <lnxawdr@web.de>
2017-10-24 19:59:09 +0200

    Format der Resultate angepasst

--- wetterserver.py
+++ wetterserver.py
@@ -143,7 +143,11 @@ def server_starten():
                                                          sensor_bmp180[2])
 
                     schnittstelle.sendall(str.encode(tempsensor +" " + drucksensor))
-
+                if anfrage[0:9] =='MESSDATEN':
+                    #schnittstelle.sendall(str.encode(" Hier sind die Daten"))
+                    sensor_dht22=sensor_DHT22_anfrage()
+                    sensor_bmp180=sensor_BMP180_anfrage()
+                    schnittstelle.sendall(str.encode(sensor_dht22,sensor_bmp180))
                 # Abbruch wenn AB gesendet wird vom client
                 if anfrage[0:2] == 'AB':
                     print("Verbindung wurde durch die Aufforderung des Client geschlosssen!")
